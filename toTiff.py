bb0d41e4125f31a9731061200077b3acf8d06206
mbarnestech <maggie.barnes@gmail.com>
2023-11-13 19:28:11 -0700

    ran tray 1 successfully with this version but with a 90 deg img rotation

--- toTiff.py
+++ toTiff.py
@@ -1,13 +1,13 @@
 from PIL import Image
 import os
 import sys
-import re
 
 path = sys.argv[1]
 tiff_path = sys.argv[2]
 directory = os.fsencode(path)
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
-    name_root = re.match(filename, r'[\w]+_[\d]+')
-    im = Image.open(filename)
+    name_root = filename.split('.')[0]
+    print(name_root, type(name_root))
+    im = Image.open(f'{path}{filename}')
     im.save(f'{tiff_path}/{name_root}.tiff', 'TIFF')
